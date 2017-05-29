0a6610076cbc184166e19bb71b6180dec16165af
Skillz <admin@ezlgg.com>
2017-05-28 21:58:22 -0400

    fixed all errors found in the JS syntax and improved some using Jquery docs with shorter better terms

--- js/app.js
+++ js/app.js
@@ -12,30 +12,29 @@ function numStudentPage () {
 }
 
 //determine which students should appear on which page using the amount of studentsPerPage and the page clicked by user.
-function showPage (currentPage, studentList) {
-  for (let i = 0; i < studentList.length; i += 1) {
-    if (i >= (currentPage * studentsPerPage) && i <= ((currentPage * studentsPerPage) + (studentsPerPage - 1)) {
-        studentList[i].show();
-  } else if (i <= currentPage * studentsPerPage) {
-    studentList[i].show();
-  } else {
-    studentList[i].hide();
+function showPage (currentPage) {
+  for (let i = 0; i <= studentList.length; i += 1) {
+    if (i >= currentPage * studentsPerPage && i <= (currentPage * studentsPerPage) + (studentsPerPage - 1)) {
+      studentList[i].show();
+    } else if (i <= currentPage * studentsPerPage) {
+      studentList[i].show();
+    } else {
+      studentList[i].hide();
+    }
   }
-}
 }
 
 //function to allow page content updates when a user clicks a pagination link
 function editPageLinks () {
   for (let i = 1; i < maxPages; i += 1) {
-    //conditional to add the active class on the page that is clicked.
     if (i == currentPage) {
-      createPageLink = <'a href="#" class="active"></a>'>
+      createPageLink = '<a href="#" class="active"></a>';
     }
     createPageLink.textContent = i;
-    createPage = createPage.appendChild(createPageLink);
-    createPageList.childNode.appendChild(createPage);    
-    }
+    createPage = createPage.append(createPageLink);
+    CreatePageList.childNode.append(createPage);
   }
+}
 
 //add HTML for page links based on max amount of students.
 function appendPageLinks (studentList) {
@@ -46,8 +45,10 @@ function appendPageLinks (studentList) {
   //call functions
   showPage(currentPage, studentList);
   appendPageLinks(studentList);
+
   //event listener to edit page links with the correct active class on the clicked page.
-  $('.pagination ul li a').on('click', () => {
-    currentPage = e().textContent();
+  $('.pagination ul li a').click(function editPages () {
+    currentPage = event.target.textContent;
     editPageLinks();
-  }
+  })
+  
