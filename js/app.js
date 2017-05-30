6b9eb04257013cbdaa43722dbc11db7f1947d55e
Skillz <admin@ezlgg.com>
2017-05-29 21:06:02 -0400

    complete but search not working

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ let name, email;
 const studentList = document.getElementsByClassName('student-item');
 let pageNum = 1;
 let studentsPerPage = 10;
-let totalPages = Math.ceil(studentList.length/studentsPerPage);
 let ul = '<ul>';
 let studentNames = $('h3');
 let studentEmails = $('.email');
@@ -11,21 +10,23 @@ let matchedStudents = [];
 
 //add search bar and page links div
 $('h2').after('<div class="student-search"> <input id="search" placeholder="Search for students..."> <button>Search</button> </div>');
-$('ul').after('<div class="pagination"></div>');
+
 
 //determine which students should appear on which page using the amount of studentsPerPage and the page clicked by user.
-function showPage (pageNum, studentList) {
+function showPage (x, y) {
   $(studentList).hide();
-  for (let i = 0; i <= studentList.length; i += 1) {
-    if (i < pageNum * studentsPerPage && i > (pageNum - 1) * studentsPerPage - 1) {
-      $(studentList[i]).show();
+  for (let i = 0; i <= y.length; i++) {
+    if (i < x * studentsPerPage && i > (x - 1) * studentsPerPage - 1) {
+      $(y[i]).show();
     } 
   }
 }
 
 // create Pagination links by checking the max amount of pages then creating the HTML elements for each page.
-function appendPageLinks (studentList) {
-  for (let i = 1; i <= totalPages; i += 1) {
+function appendPageLinks (x) {
+  let totalPages = Math.ceil(x.length/studentsPerPage);
+  $('ul').after('<div class="pagination"></div>');
+  for (let i = 1; i <= totalPages; i++) {
     let tempVar ='<li><a href="#">' + i + '</a></li>';
     ul = ul + tempVar;
   }
@@ -34,24 +35,33 @@ function appendPageLinks (studentList) {
 //event listener to start showPage function when a link is clicked on.
   $("a").click(function(){
     pageNum = event.target.textContent;
-    showPage(pageNum, studentList);
+    showPage(pageNum, x);
   });
 }
 
 //search function to search for student and filter all the lists and pagination based on search
 function studentSearch () {
-   let search = $('#search').val();
-   search = search.toLowerCase();
+   let search = document.getElementById('search').value.toLowerCase();
    $('.pagination').empty();
-   for (let i = 0; i <= studentList.length; i += 1) {
+   document.getElementById('search').value = '';
+   for (var i = 0; i < studentList.length; i++) {
      name = studentNames[i].textContent;
       email = studentEmails[i].textContent;
-     if (name.match(search) || email.match(search)) {
+     if (name.search(search) != -1  || email.search(search) != -1) {
        matchedStudents.push(studentList[i]);
      } 
    }
+   $('div').remove('.pagination');
+   console.log(matchedStudents);
+   if (matchedStudents.length == 0) {
+     $(studentList).hide();
+     $('.student-list').after('<h4> Error: Search Not Found! </h4><p>Either the student searched for does not exist in the database or the search criteria was misspelled. Please try again.</p>');
+  } else if (matchedStudents.length < 10 && matchedStudents.length > 0) {
+     showPage(1, matchedStudents);
+ } else {
    showPage(1, matchedStudents);
    appendPageLinks(matchedStudents);
+ }
 }
 
 showPage(1, studentList);
