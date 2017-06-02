7ba321a58ae54eee428ac878c2bd593310c2cddd
Skillz4Killz <gankstarsleague@gmail.com>
2017-06-02 12:12:17 -0400

    improving comments to allow to understand better

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ function showPage (x, y) {
 function appendPageLinks (x) {
   let totalPages = Math.ceil(x.length/studentsPerPage);
   $('ul').after('<div class="pagination"></div>');
+  //reset pagination ul variable for multiple searches without refreshing.
   ul = '<ul>';
   for (let i = 1; i <= totalPages; i++) {
     let tempVar ='<li><a href="#">' + i + '</a></li>';
@@ -32,14 +33,14 @@ function appendPageLinks (x) {
   }
   ul = ul + '</ul>';
   $('.pagination').append(ul);
-//event listener to start showPage function when a link is clicked on.
+//event listener to start showPage function when a link is clicked on using the parameter of studentList/matchedStudents list.
   $("a").click(function(){
     pageNum = event.target.textContent;
     showPage(pageNum, x);
   });
 }
 
-//search function to search for student and filter all the lists and pagination based on search
+/* search for student & filter lists & pagination based on search and add all matched students into an array. The search will find any character in the entire name or email. For example searching for `hi` will produce 2 results that contain those letters inside it. */
 function studentSearch () {
    let search = document.getElementById('search').value.toLowerCase();
    $('.pagination').remove();
@@ -52,22 +53,26 @@ function studentSearch () {
        matchedStudents.push(studentList[i]);
      } 
    }
-   
-   console.log(matchedStudents);
+  // decide what to do once matchedList is complete based on how many students in the list.
    if (matchedStudents.length == 0) {
      $(studentList).hide();
+     //Hide multiple errors on multiple error searches
      $('.error').remove();
+     //show error
      $('.student-list').after('<h4 class="error"> Error: Search Not Found! </h4><p class="error">Either the student searched for does not exist in the database or the search criteria was misspelled. Please try again.</p>');
   } else if (matchedStudents.length < 10 && matchedStudents.length > 0) {
      showPage(1, matchedStudents);
-     $('.error').remove();
+     //remove error in previous searches that result in error
+    $('.error').remove();
  } else {
+   //remove error in previous searches that result in error
    $('.error').remove();
    showPage(1, matchedStudents);
    appendPageLinks(matchedStudents);
  }
 }
 
+//Functions to call that are required to set the page up once loaded.
 showPage(1, studentList);
 appendPageLinks(studentList);
 $("button").click(studentSearch);
